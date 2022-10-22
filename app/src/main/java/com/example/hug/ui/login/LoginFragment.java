47ead3416cdcf5a9866f7c7360ad1e55b1e53572
fdocharles <fdocharles@gmail.com>
2022-10-22 09:05:36 -0400

    Added Sign up view and linked the login and signup buttons to correct destinations

--- app/src/main/java/com/example/hug/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/hug/ui/login/LoginFragment.java
@@ -1,6 +1,7 @@
 package com.example.hug.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.graphics.drawable.ColorDrawable;
@@ -9,16 +10,20 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.hug.R;
+import com.example.hug.ui.signup.SignUpFragment;
+import com.google.android.material.button.MaterialButton;
 
 public class LoginFragment extends Fragment {
 
     private LoginViewModel mViewModel;
+    private MaterialButton signUpBtn;
 
     public static LoginFragment newInstance() {
         return new LoginFragment();
@@ -27,14 +32,16 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-
-
         ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
 
 //        ((AppCompatActivity) getActivity()).getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.primary)));
 
 
         View view =  inflater.inflate(R.layout.fragment_login, container, false);
+
+        signUpBtn  = view.findViewById(R.id.login_sign_up_btn);
+
+        signUpBtn.setOnClickListener(signUpBtnClickListener());
 
 
         return view;
@@ -47,5 +54,13 @@ public class LoginFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+    private View.OnClickListener signUpBtnClickListener(){
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_navigation_login_to_navigation_signup);
+            }
+        };
+    }
 
 }
--- app/src/main/java/com/example/hug/ui/signup/SignUpFragment.java
+++ app/src/main/java/com/example/hug/ui/signup/SignUpFragment.java
@@ -7,16 +7,20 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavOptions;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.hug.R;
+import com.google.android.material.button.MaterialButton;
 
 public class SignUpFragment extends Fragment {
 
     private SignUpViewModel mViewModel;
+    private MaterialButton loginBtn;
 
     public static SignUpFragment newInstance() {
         return new SignUpFragment();
@@ -25,7 +29,12 @@ public class SignUpFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        loginBtn = view.findViewById(R.id.signup_login_btn);
+
+        loginBtn.setOnClickListener(loginBtnClickListener());
+
+        return view;
     }
 
     @Override
@@ -33,6 +42,15 @@ public class SignUpFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(SignUpViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    private View.OnClickListener loginBtnClickListener(){
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_navigation_signup_to_navigation_login);
+            }
+        };
     }
 
 }
